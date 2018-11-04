171e8793cd3dd111afb71c591ce76e98968afa70
Brad Schrag <brad.schrag@gmail.com>
2018-11-04 22:58:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.count == 0
+    message = "The line is currenly empty."
+  else
+    message = "The line is currently: "
+    i = 1
+    array.each do | name |
+      message += "#{i}. #{name} "
+      i += 1
+    end
+  message
+end
